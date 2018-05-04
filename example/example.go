200d61e432b6a08d64fd04d897b5c11c35ad22b3
Nathan J Cochran <nathanjcochran@gmail.com>
2018-05-03 22:06:59 -0400

    Change repo/package name to mock

--- example/example.go
+++ example/example.go
@@ -6,7 +6,7 @@ import (
 	renamed "text/template"
 )
 
-//go:generate mocker -o mock.go MyInterface
+//go:generate mock -o mock.go MyInterface
 type MyInterface interface {
 	MyMethod1(name string, _ int) (err1, err2 error)
 	MyMethod2(templ template.Template) (out struct{ OutTemplate renamed.Template })
