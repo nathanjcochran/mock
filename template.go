e8e40ff7354f3fa9c68ba96d6729006a772ce5e0
Nathaniel J Cochran <nathanjcochran@gmail.com>
2019-05-16 17:56:50 -0400

    Better mock implementation

--- template.go
+++ template.go
@@ -8,21 +8,22 @@ import (
 	{{- end }}
 )
 
+{{ range $method := .Methods -}}
+type {{ $.Name }}{{ .Name }}Args struct {
+	{{ .Params.NamedFields }}
+}
+type {{ $.Name }}{{ .Name }}Results struct {
+	{{ .Results.NamedFields }}
+}
+{{ end }}
+
 type {{ .Name }}Mock struct {
 	T *testing.T
 	{{- range .Methods }}
-	{{ .Name }}Func struct{
-		Expect []struct{
-			{{ .Params.NamedFields }}
-		}
-		Return []struct{
-		    {{ .Results.NamedFields }}
-		}
-		Calls []struct{
-			{{ .Params.NamedFields }}
-		}
-		lock sync.Mutex
-	}
+	{{ .Name }}Expects []{{ $.Name }}{{ .Name }}Args
+	{{ .Name }}Returns []{{ $.Name }}{{ .Name}}Results
+	{{ .Name }}Calls []{{ $.Name }}{{ .Name }}Args
+	{{ .Name }}Lock sync.Mutex
 	{{- end }}
 }
 
@@ -31,29 +32,27 @@ var _ {{ .Name }} = &{{ .Name }}Mock{}
 {{- range $method := .Methods }}
 
 func (m *{{ $.Name }}Mock) {{ .Name }}({{ .Params.NamedString }}) {{ .Results }}{
-	m.{{ .Name }}Func.lock.Lock()
-	defer m.{{ .Name }}Func.lock.Unlock()
+	m.{{ .Name }}Lock.Lock()
+	defer m.{{ .Name }}Lock.Unlock()
 
-	m.{{ .Name }}Func.Calls = append(m.{{ .Name }}Func.Calls, struct{
-		{{ .Params.NamedFields }}
-	}{ {{ .Params.ArgsString }} })
+	m.{{ .Name }}Calls = append(m.{{ .Name }}Calls, {{ $.Name }}{{ .Name }}Args{ {{ .Params.ArgsString }} })
 
-	if m.T != nil && m.{{ .Name }}Func.Expect != nil {
-		if len(m.{{ .Name }}Func.Calls) > len(m.{{ .Name }}Func.Expect) {
+	if m.T != nil && m.{{ .Name }}Expects != nil {
+		if len(m.{{ .Name }}Calls) > len(m.{{ .Name }}Expects) {
 			m.T.Errorf("Unexpected call to {{ $.Name }}.{{ .Name }}. Expected calls: %d. Actual: %d",
-				len(m.{{ .Name }}Func.Expect), len(m.{{ .Name }}Func.Calls))
+				len(m.{{ .Name }}Expects), len(m.{{ .Name }}Calls))
 		} else {
-			expected := m.{{ .Name }}Func.Expect[len(m.{{ .Name }}Func.Calls) - 1]
-			{{ range $index, $element := .Params }}
+			expected := m.{{ .Name }}Expects[len(m.{{ .Name }}Calls) - 1]
+			{{- range $index, $element := .Params }}
 			if !reflect.DeepEqual(expected.{{ .FieldName $index }}, {{ .Named $index }}){
 				m.T.Errorf("{{ $.Name }}.{{ $method.Name }} expected {{ .Named $index }}: %v. Actual: %v", expected.{{ .FieldName $index}}, {{ .Named $index }})
 			}
-			{{ end }}
+			{{- end }}
 		}
 	}
 
 	{{- if gt (len .Results) 0 }}
-	results := m.{{ .Name }}Func.Return[len(m.{{ .Name }}Func.Calls) - 1]
+	results := m.{{ .Name }}Returns[len(m.{{ .Name }}Calls) - 1]
 	return {{ .Results.ReturnList "results" }}
 	{{- end }}
 }
