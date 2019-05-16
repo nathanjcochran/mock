a8849e65a339ae6cc3707363ff37e554ec6bb697
Nathaniel J Cochran <nathanjcochran@gmail.com>
2019-05-16 11:03:18 -0400

    Use go imports  instead of go fmt to process file (groups import statements better)

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
-	"go/format"
 	"log"
 	"os"
 	"text/template"
 
 	"github.com/nathanjcochran/mock/iface"
+	"golang.org/x/tools/imports"
 )
 
 func main() {
@@ -24,38 +24,38 @@ func main() {
 	}
 	flag.Parse()
 
-	// First argument is interface name:
+	// First argument is interface name
 	args := flag.Args()
 	if len(args) < 1 {
 		log.Fatal("Not enough args")
 	}
 	ifaceName := args[0]
 
-	// Parse the package and get info about the interface:
+	// Parse the package and get info about the interface
 	iface, err := iface.GetInterface(*dir, ifaceName)
 	if err != nil {
 		log.Fatalf("Error getting interface information: %s", err)
 	}
 
-	// Parse the template:
+	// Parse the template
 	tmpl, err := template.New("default").Parse(tmpl)
 	if err != nil {
 		log.Fatalf("Error parsing template: %s", err)
 	}
 
-	// Execute/output the template:
+	// Execute/output the template
 	buf := &bytes.Buffer{}
 	if err := tmpl.Execute(buf, &iface); err != nil {
 		log.Fatalf("Error executing template: %s", err)
 	}
 
-	// Format it with go fmt:
-	formatted, err := format.Source(buf.Bytes())
+	// Format it with go imports
+	formatted, err := imports.Process(*outFile, buf.Bytes(), nil)
 	if err != nil {
 		log.Fatalf("Error formating output: %s", err)
 	}
 
-	// Open the file, if provided, or use stdout:
+	// Open the file, if provided, or use stdout
 	out := os.Stdout
 	if *outFile != "" {
 		out, err = os.Create(*outFile)
@@ -65,7 +65,7 @@ func main() {
 		defer out.Close()
 	}
 
-	// Write the formatted output to the file:
+	// Write the formatted output to the file
 	if _, err := out.Write(formatted); err != nil {
 		log.Fatalf("Error writing to file: %s", err)
 	}
